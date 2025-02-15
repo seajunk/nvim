a4154108d3392006507b532c939b82c8f7dbe788
seajunk <seajun.kim@mail.utoronto.ca>
2025-02-15 07:50:30 -0500

    new stuff

--- lua/keybinds.lua
+++ lua/keybinds.lua
@@ -1,11 +1,69 @@
--- Normal mode keybinding
---vim.api.nvim_set_keymap('n', '<F1>', ':HopWord<CR>', { noremap = true, silent = true })
-vim.g.mapleader = " " 
 
-
-
---vim.api.nvim_set_keymap('n', '<M-t>', ':FloatermNew --height=0.9 --width=0.9 <CR>', { noremap = true, silent = true })
---vim.api.nvim_set_keymap('t', '<m-t>', '<C-\\><C-n>:FloatermKill<CR>', {noremap = true, silent = true})
-
+-- floatterm stuff
 vim.api.nvim_set_keymap('n', '<M-t>', ':FloatermToggle<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('t', '<M-t>', '<C-\\><C-n>:FloatermToggle<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('t', '<Esc>', '<C-\\><C-n>', { noremap = true, silent = true })
+
+-- nvimtree stuff
+vim.api.nvim_set_keymap('n', '\t', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
+
+-- telescope stuff
+vim.api.nvim_set_keymap('n', '<F1>', ':Telescope find_files<Cr>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<F2>', ':Telescope buffers<Cr>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<F3>', ':Telescope live_grep<Cr>', { noremap = true, silent = true })
+
+
+
+
+
+-- bufferline stuff
+--[[
+vim.api.nvim_set_keymap('n', '<Backspace>', ':bdelete<Cr>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<M-1>', ':BufferLineGoToBuffer 1<Cr>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<M-2>', ':BufferLineGoToBuffer 2<Cr>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<M-3>', ':BufferLineGoToBuffer 3<Cr>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<M-4>', ':BufferLineGoToBuffer 4<Cr>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<M-5>', ':BufferLineGoToBuffer 5<Cr>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<M-6>', ':BufferLineGoToBuffer 6<Cr>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<M-7>', ':BufferLineGoToBuffer 7<Cr>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<M-8>', ':BufferLineGoToBuffer 8<Cr>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<M-9>', ':BufferLineGoToBuffer 9<Cr>', { noremap = true, silent = true })
+--]]
+
+-- tab stuff
+vim.api.nvim_set_keymap('n', '<M-c>', ':tabclose<Cr>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<M-n>', ':tabnew<Cr>', { noremap = true, silent = true })
+
+vim.api.nvim_set_keymap('n', '<M-1>', '1gt', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<M-2>', '2gt', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<M-3>', '3gt', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<M-4>', '4gt', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<M-5>', '5gt', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<M-6>', '6gt', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<M-7>', '7gt', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<M-8>', '8gt', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<M-9>', '9gt', { noremap = true, silent = true })
+
+-- window stuff
+vim.api.nvim_set_keymap('n', '<M-h>', '<C-w>h', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<M-j>', '<C-w>j', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<M-k>', '<C-w>k', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<M-l>', '<C-w>l', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<M-q>', '<C-w>q', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<M-H>', '<C-w><', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<M-J>', '<C-w>-', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<M-K>', '<C-w>+', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<M-L>', '<C-w>>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<M-q>', '<C-w>q', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<M-v>', ':vs<Cr>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<M-s>', ':sp<Cr>', { noremap = true, silent = true })
+
+
+
+
+
+
+
+
+
+
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,119 +1,187 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
-      { "\nPress any key to exit..." },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
+    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+    local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+    if vim.v.shell_error ~= 0 then
+        vim.api.nvim_echo({
+            { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+            { out, "WarningMsg" },
+            { "\nPress any key to exit..." },
+        }, true, {})
+        vim.fn.getchar()
+        os.exit(1)
+    end
 end
+
 vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
-  {       "https://github.com/morhetz/gruvbox.git",
-  config = function()
-    vim.cmd("colorscheme gruvbox")
-  end
+    {       "https://github.com/morhetz/gruvbox.git",
+    config = function()
+        vim.cmd("colorscheme gruvbox")
+    end
 },
 {
     "https://github.com/voldikss/vim-floaterm.git",
 
 },
 {
-  "https://github.com/vim-airline/vim-airline.git",
+    "https://github.com/vim-airline/vim-airline.git",
 },
 {
-  "https://github.com/nvim-treesitter/nvim-treesitter.git",
-  config = function() 
-    require("nvim-treesitter.configs").setup({
-      highight = {
-        enable = true
-      },
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = "<Leader>ss", -- set to `false` to disable one of the mappings
-          node_incremental = "<Leader>si",
-          scope_incremental = "<Leader>sc",
-          node_decremental = "<Leader>sd",
-        },
-      },
-    })
-  end
-
-},
-{
-  "https://github.com/neovim/nvim-lspconfig.git",
-  config = function()
-    local lspconfig = require("lspconfig")
-    lspconfig.clangd.setup({})
-    lspconfig.asm_lsp.setup({})
-  end
-
-},
-{
-    "williamboman/mason.nvim",
-    config = function()
-        local masonconfig = require("mason").setup();
+    "https://github.com/nvim-treesitter/nvim-treesitter.git",
+    config = function() 
+        require("nvim-treesitter.configs").setup({
+            highight = {
+                enable = true
+            },
+            incremental_selection = {
+                enable = true,
+                keymaps = {
+                    init_selection = "<Leader>ss", -- set to `false` to disable one of the mappings
+                    node_incremental = "<Leader>si",
+                    scope_incremental = "<Leader>sc",
+                    node_decremental = "<Leader>sd",
+                },
+            },
+        })
     end
 
 },
---[[
 {
-  "folke/flash.nvim",
-  event = "VeryLazy",
-  ---@type Flash.Config
-  opts = {},
-  -- stylua: ignore
-  keys = {
-    { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" }
-  },
-},
---]]
---[[
-{
-  "lervag/vimtex",
-  lazy = false,     -- we don't want to lazy load VimTeX
-  -- tag = "v2.15", -- uncomment to pin to a specific release
-  init = function()
-    -- VimTeX configuration goes here, e.g.
-    vim.g.vimtex_view_method = "zathura"
-  end
-}
---]]
---[[
-{
-  "https://github.com/ggandor/leap.nvim",
-  config = function()
-    require('leap')
-  end
-}
---]]
---[[
-{
-  'smoka7/hop.nvim',
-  version = "*",
-  opts = {
-    keys = 'etovxqpdygfblzhckisuran'
-  }
-}
---]]
---[[
-{
-  "https://github.com/jinh0/eyeliner.nvim.git",
-  config = function()
-    require("eyeliner").setup({
-      highlight_on_key = true,
-      dim = false,
-      max_length = 9999,
-      default_keymaps = true
+    "https://github.com/neovim/nvim-lspconfig.git",
+    config = function()
+        local lspconfig = require("lspconfig")
+        lspconfig.clangd.setup({})
+        --lspconfig.asm_lsp.setup({})
+    end
 
-    })
-  end
-}
+},
+{
+    "https://github.com/nvim-tree/nvim-tree.lua",
+    config = function()
+        local api = require("nvim-tree.api")
+
+        local function my_on_attach(bufnr)
+
+            local function opts(desc)
+                return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+            end
+            vim.keymap.set("n", "<Cr>", api.node.open.tab)
+        end
+
+        -- opens buffer but keeps focus on tree
+        local function open()
+            api.node.open.edit(node)
+            api.tree.focus()            
+        end
+        vim.keymap.set("n", "<Space>", open)
+
+        require("nvim-tree").setup{
+            renderer = {
+                icons = {
+                    git_placement = "before",
+                    modified_placement = "after",
+                    hidden_placement = "after",
+                    diagnostics_placement = "signcolumn",
+                    bookmarks_placement = "signcolumn",
+                    padding = " ",
+                    symlink_arrow = " ➛ ",
+                    show = {
+                        file = false,
+                        folder = false,
+                        folder_arrow = true,
+                        git = true,
+                        modified = true,
+                        hidden = false,
+                        diagnostics = true,
+                        bookmarks = true,
+		},
+	},
+},           
+update_focused_file = {
+	enable = true,
+	update_root = {
+		enable = false,
+		ignore_list = {},
+	},
+	exclude = false,
+},
+on_attach = my_on_attach,
+	} 
+end
+},
+{
+    "https://github.com/nvim-lua/plenary.nvim",
+},
+{
+    "https://github.com/nvim-treesitter/nvim-treesitter",
+    config = function()
+        require'nvim-treesitter.configs'.setup {
+            -- A list of parser names, or "all" (the listed parsers MUST always be installed)
+            ensure_installed = { "c", "cpp"},
+
+            -- Install parsers synchronously (only applied to `ensure_installed`)
+            sync_install = false,
+
+            -- Automatically install missing parsers when entering buffer
+            -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+            auto_install = true,
+
+            -- List of parsers to ignore installing (or "all")
+            ignore_install = { "javascript" },
+
+            ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
+            -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
+
+            highlight = {
+                enable = true,
+
+                -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
+                -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
+                -- the name of the parser)
+                -- list of language that will be disabled
+                disable = {},
+                -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
+            },
+        }
+    end
+},
+{
+    "https://github.com/nvim-telescope/telescope.nvim",
+    config = function()
+        local actions = require("telescope.actions")
+
+        require("telescope").setup({
+            defaults = {
+                file_previewer = require'telescope.previewers'.vim_buffer_cat.new,
+                mappings = {
+                    i = {
+                        ["<F1>"] = actions.close,
+                        ["<F2>"] = actions.close,
+                        ["<F3>"] = actions.close,
+                    },
+                },
+                {},
+            },
+        })
+    end
+},
+--[[
+{
+    "https://github.com/akinsho/bufferline.nvim",
+    config = function()
+        require("bufferline").setup{
+            options = {
+                --mode = "tabs",
+                show_buffer_icons = false,
+                show_buffer_close_icons = false,
+                show_close_icon = false,
+                show_tab_indicators = true,
+                number = "ordinal",
+                seperator_style = "slope"
+            }
+        }
+    end
+},
 --]]
 })
